a014e8a78ed9aa5e08d393f351f437aeb855e75a
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-11-16 00:33:45 -0300

    Configurações gerais

---
+++ beck_end/src/main/java/br/com/miribekah/config/ControleExcecoes.java
@@ -0,0 +1,107 @@
+package br.com.miribekah.config;
+
+import br.com.miribekah.dto.ObjetoErroDTO;
+import br.com.miribekah.service.SendEnvioEmailService;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
+import java.util.List;
+
+@RestControllerAdvice
+@ControllerAdvice
+public class ControleExcecoes extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private SendEnvioEmailService sendEnvioEmailService;
+
+    private String msg = "";
+
+    /*Captura excessões do projeto*/
+    @ExceptionHandler({Exception.class, RuntimeException.class, Throwable.class})
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+        ObjetoErroDTO objetoErroDTO = new ObjetoErroDTO();
+
+        if (ex instanceof MethodArgumentNotValidException){
+            List<ObjectError> list = ((MethodArgumentNotValidException) ex).getBindingResult().getAllErrors();
+            for (ObjectError objectError : list) {
+                msg += objectError.getDefaultMessage() + "\n";
+            }
+        } else if(ex instanceof HttpMessageNotReadableException) {
+            msg += "Não esta sendo enviado dados no corpo da requisição(BodY) ";
+
+        }else{
+            msg = ex.getMessage();
+        }
+        objetoErroDTO.setError(msg);
+        objetoErroDTO.setCod(status.value() + " ==> " + status.getReasonPhrase());
+
+        ex.printStackTrace();
+        try {
+            sendEnvioEmailService.enviarEmailHtml("Erro na Loja Confeitaria Miribekah", ExceptionUtils.getStackTrace(ex)
+                    , "juliocesar.coutinhodev@gmail.com");
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new ResponseEntity<Object>(objetoErroDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /*Caputura erro de banco*/
+    @ExceptionHandler({DataIntegrityViolationException.class, ConstraintViolationException.class, SQLException.class})
+    protected ResponseEntity<Object> handleExceptionDataIntegry(Exception ex){
+
+        ObjetoErroDTO objetoErroDTO = new ObjetoErroDTO();
+        if (ex instanceof DataIntegrityViolationException){
+            msg = "Erro de Integridade no banco " + ex.getCause().getMessage();
+        }else if (ex instanceof ConstraintViolationException){
+            msg = "Chave estrangeira violada + " + ex.getCause().getMessage();
+        }else if (ex instanceof SQLException){
+            msg = "Erro de banco de dados " + ex.getCause().getMessage();
+        }else {
+            msg = ex.getMessage();
+        }
+
+        objetoErroDTO.setError(msg);
+        objetoErroDTO.setCod(HttpStatus.INTERNAL_SERVER_ERROR.toString());
+
+        ex.printStackTrace();
+        try {
+            sendEnvioEmailService.enviarEmailHtml("Erro na Loja MimoBella", ExceptionUtils.getStackTrace(ex)
+                    , "julio@gemmap.com.br");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new ResponseEntity<Object>(objetoErroDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ExcepetionJava.class)
+    public ResponseEntity<Object> handleExceptionCustom(ExcepetionJava ex){
+        ObjetoErroDTO objetoErroDTO = new ObjetoErroDTO();
+        objetoErroDTO.setError(ex.getMessage());
+        objetoErroDTO.setCod(HttpStatus.OK.toString());
+
+        return new ResponseEntity<Object>(objetoErroDTO, HttpStatus.OK);
+    }
+
+}
---
+++ beck_end/src/main/java/br/com/miribekah/service/SendEnvioEmailService.java
@@ -0,0 +1,46 @@
+package br.com.miribekah.service;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+@Service
+public class SendEnvioEmailService {
+    public String userName = "miribekah@outlook.com";
+    public String senha = "miribekah147258#";
+
+    @Async
+    public void enviarEmailHtml(String assunto, String mensagem, String emailDestino) throws MessagingException, UnsupportedEncodingException {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.office365.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, senha);
+            }
+        });
+
+        session.setDebug(true);
+
+        Address [] toUser = InternetAddress.parse(emailDestino);
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(userName, "Miribekah", "UTF-8"));
+        message.setRecipients(Message.RecipientType.TO, toUser);
+        message.setSubject(assunto);
+        message.setContent(mensagem, "text/html; charset=utf-8");
+
+        Transport transport = session.getTransport("smtp");
+        transport.connect(userName, senha);
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
+    }
+}
